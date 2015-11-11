8a33bdcd78920ad14387589586cab33c3ded6e97
Michal Markiewicz <markiewicz.michal@hotmail.com>
2015-11-11 19:18:35 +0000

    Added primes multiplication logic. Added passing tests for GridMultiplicator.Calculate()

--- PrimeNumbers/PrimeNumbers.Core/GridMultiplicator.cs
+++ PrimeNumbers/PrimeNumbers.Core/GridMultiplicator.cs
@@ -1,13 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PrimeNumbers.Core
 {
     public class GridMultiplicator
     {
-        public IEnumerable Calculate(IEnumerable<int> values)
+        public int[,] Calculate(List<int> values)
         {
-            throw new System.NotImplementedException();
+            var rowSize = values.Count + 1;
+            var grid = new int[rowSize, rowSize];
+
+            for (int i = 0; i < rowSize; i++)
+            {
+                for (int j = 0; j < rowSize; j++)
+                {
+                    if (i == 0)
+                    {
+                        if (j == 0)
+                        {
+                            grid[i, j] = -1;
+                            continue;
+                        }
+
+                        grid[i, j] = values[j - 1];
+                    }
+                    else
+                    {
+                        if (j == 0)
+                        {
+                            grid[i, j] = values[i - 1];
+                            continue;
+                        }
+
+                        grid[i, j] = values[i - 1] * values[j - 1];
+                    }
+                }
+            }
+
+            return grid;
         }
     }
 }
--- PrimeNumbers/PrimeNumbers.UnitTests/PrimeNumbers.Core/GridMultiplicatorTests.cs
+++ PrimeNumbers/PrimeNumbers.UnitTests/PrimeNumbers.Core/GridMultiplicatorTests.cs
@@ -8,10 +8,22 @@ namespace PrimeNumbers.UnitTests.PrimeNumbers.Core
     public class GridMultiplicatorTests
     {
         [Test]
-        public void Calculate_MultiplyRowsWithCollumns_ReturnMultiArray()
+        public void Calculate_TwoPrimesInCollection_MultiplyRowsWithCollumnsAndReturnMultiArray()
         {
-            var expected = new int[][] { new[] { 2, 3 }, new[] { 2, 4, 6 }, new[] { 3, 6, 9 } };
+            var expected = new[,] { { -1, 2, 3 }, { 2, 4, 6 }, { 3, 6, 9 } };
             var data = new List<int>() { 2, 3 };
+            var sut = new GridMultiplicator();
+
+            var result = sut.Calculate(data);
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Calculate_ThreePrimesInCollection_MultiplyRowsWithCollumnsAndReturnMultiArray()
+        {
+            var expected = new[,] { { -1, 3, 5, 7 }, { 3, 9, 15, 21 }, { 5, 15, 25, 35 }, { 7, 21, 35, 49 } };
+            var data = new List<int>() { 3, 5, 7 };
             var sut = new GridMultiplicator();
 
             var result = sut.Calculate(data);
