d9988a26020fa1731c4960a27227700cf8f90970
Michal Markiewicz <markiewicz.michal@hotmail.com>
2015-11-11 19:37:07 +0000

    Added GridFormatter class. Added first failing test for GridFormatter.Formatt()

---
+++ PrimeNumbers/PrimeNumbers.Core/GridFormatter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeNumbers.Core
+{
+    public class GridFormatter
+    {
+        public string Formatt(int[,] grid)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PrimeNumbers/PrimeNumbers.UnitTests/PrimeNumbers.Core/GridFormatterTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+using NUnit.Framework;
+using PrimeNumbers.Core;
+
+namespace PrimeNumbers.UnitTests.PrimeNumbers.Core
+{
+    [TestFixture]
+    public class GridFormatterTests
+    {
+        [Test]
+        public void Formatt_TwoDimensionalArray_ReturnFormattedString()
+        {
+            var expected = "";
+            var array = new int[,] { { -1, 2 }, { 2, 4 } };
+            var sut = new GridFormatter();
+
+            var result = sut.Formatt(array);
+            
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
