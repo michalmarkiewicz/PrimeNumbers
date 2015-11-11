eb10bf93ab0ddf84e8cff25bd588f960a0c1830d
Michal Markiewicz <markiewicz.michal@hotmail.com>
2015-11-11 21:40:14 +0000

    Added Unity Container. Added bootstraper class to register types in container. Added tests to check if types are correctly registered

---
+++ PrimeNumbers/PrimeNumbers.ConsoleApp/ContainerBootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Practices.Unity;
+using PrimeNumbers.Contracts;
+using PrimeNumbers.Core;
+
+namespace PrimeNumbers.ConsoleApp
+{
+    public class ContainerBootstrapper
+    {
+        public void Configure(IUnityContainer container)
+        {
+            container.RegisterType<IArrayFormatter, GridFormatter>();
+            container.RegisterType<IGridMultiplicator, GridMultiplicator>();
+            container.RegisterType<IPrimeNumberGenerator, PrimeNumberGenerator>(new InjectionConstructor(10));
+        }
+    }
+}
---
+++ PrimeNumbers/PrimeNumbers.IntegrationTests/PrimeNumbers.ConsoleApp/ContainerBootstrapperTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Practices.Unity;
+using NUnit.Framework;
+using PrimeNumbers.ConsoleApp;
+using PrimeNumbers.Contracts;
+using PrimeNumbers.Core;
+
+namespace PrimeNumbers.IntegrationTests.PrimeNumbers.ConsoleApp
+{
+    [TestFixture]
+    public class ContainerBootstrapperTests
+    {
+        [Test]
+        public void Configure_IArrayFormatter_RegisterTypes()
+        {
+            var sut = new UnityContainer();
+            new ContainerBootstrapper().Configure(sut);
+
+            var result = sut.Resolve<IArrayFormatter>();
+
+            Assert.IsInstanceOf<GridFormatter>(result);
+        }
+
+        [Test]
+        public void Configure_IGridMultiplicator_RegisterTypes()
+        {
+            var sut = new UnityContainer();
+            new ContainerBootstrapper().Configure(sut);
+
+            var result = sut.Resolve<IGridMultiplicator>();
+
+            Assert.IsInstanceOf<GridMultiplicator>(result);
+        }
+
+        [Test]
+        public void Configure_IPrimeNumberGenerator_RegisterTypes()
+        {
+            var sut = new UnityContainer();
+            new ContainerBootstrapper().Configure(sut);
+
+            var result = sut.Resolve<IPrimeNumberGenerator>();
+
+            Assert.IsInstanceOf<PrimeNumberGenerator>(result);
+        }
+
+        [Test]
+        public void Configure_PrimeNumberEngine_RegisterTypes()
+        {
+            var sut = new UnityContainer();
+            new ContainerBootstrapper().Configure(sut);
+
+            var result = sut.Resolve<PrimeNumberEngine>();
+
+            Assert.IsInstanceOf<PrimeNumberEngine>(result);
+        }
+    }
+}
