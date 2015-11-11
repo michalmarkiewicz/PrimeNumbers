e16845e0f4518b0d5fd7153d78ca8030d129a833
Michal Markiewicz <markiewicz.michal@hotmail.com>
2015-11-11 16:09:05 +0000

    Added working logic for checking if number is prime

--- PrimeNumbers/PrimeNumbers.Core/PrimeNumberGenerator.cs
+++ PrimeNumbers/PrimeNumbers.Core/PrimeNumberGenerator.cs
@@ -4,12 +4,23 @@
     {
         public bool IsPrime(int number)
         {
-            if (number == 4)
+            if ((number & 1) == 0)
             {
+                if (number == 2)
+                    return true;
+
                 return false;
             }
 
-            return true;
+            for (int i = 3; (i * i) <= number; i += 2)
+            {
+                if ((number % i) == 0)
+                {
+                    return false;
+                }
+            }
+
+            return number != 1;
         }
     }
 }
--- PrimeNumbers/PrimeNumbers.UnitTests/PrimeNumbers.Core/PrimeNumberGeneratorTests.cs
+++ PrimeNumbers/PrimeNumbers.UnitTests/PrimeNumbers.Core/PrimeNumberGeneratorTests.cs
@@ -14,18 +14,29 @@ namespace PrimeNumbers.UnitTests.PrimeNumbers.Core
             sut = new PrimeNumberGenerator();
         }
 
-        [Test]
-        public void IsPrime_NumberIsPrime_ReturnTrue()
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(41)]
+        [TestCase(409)]
+        [TestCase(691)]
+        [TestCase(887)]
+        [TestCase(533000389)]
+        public void IsPrime_NumberIsPrime_ReturnTrue(int number)
         {
-            var result = sut.IsPrime(2);
+            var result = sut.IsPrime(number);
 
             Assert.True(result);
         }
 
-        [Test]
-        public void IsPrime_NumberIsNotPrime_ReturnFalse()
+        [TestCase(4)]
+        [TestCase(6)]
+        [TestCase(222)]
+        [TestCase(125)]
+        [TestCase(678)]
+        [TestCase(382754)]
+        public void IsPrime_NumberIsNotPrime_ReturnFalse(int number)
         {
-            var result = sut.IsPrime(4);
+            var result = sut.IsPrime(number);
 
             Assert.IsFalse(result);
         }
